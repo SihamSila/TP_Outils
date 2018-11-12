be010bdb93b89bd3ea4b3ca60d5edc7a0f726a62
Berrichi <fs_berrichi@esi.dz>
2018-11-12 10:04:55 +0100

    Modification de UserController

--- src/com/telly/controllers/UserController.java
+++ src/com/telly/controllers/UserController.java
@@ -13,55 +13,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.telly.dao.FormValidationGroup;
 import com.telly.dao.Reserve;
-/*import com.telly.dao.User;*/
+
 import com.telly.service.ReserveService;
-/*import com.telly.service.UserService;*/
+
 
 
 
 @Controller
 public class UserController {
 
-	/*@Autowired
-	UserService userService;*/
+
 	
 	@Autowired
 	ReserveService reserveService;
 
-	/*@RequestMapping("/login")
-	public String showLogin() {
-		return "login";
-	}
 	
-	@RequestMapping("/loggedout")
-	public String showLogout() {
-		return "loggedout";
-	}
-	
-	
-	@RequestMapping("/createaccount")
-	public String createAccount(Model model, Principal principal) {
-		
-		model.addAttribute("user", new User());
-		
-		return "createaccount";
-	}
-
-	@RequestMapping(value = "/createuser", method = RequestMethod.POST)
-	public String createUser(@Validated(FormValidationGroup.class) User user, BindingResult result) {
-		
-		if(result.hasErrors()) {
-			return "createaccount";
-		}
-		
-		user.setAuthority("ROLE_USER");
-		user.setEnabled(true);
-
-		userService.create(user);
-		
-		return "home";
-
-	}*/
 
 	@RequestMapping(value = "/reservebook", method = RequestMethod.POST)
 	public String createReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, BindingResult result, Principal principal) {
